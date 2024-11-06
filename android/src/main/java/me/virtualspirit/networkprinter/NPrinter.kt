31d291b0fc7e3f8c63aa5c06f43f037886d7c282
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2024-11-06 13:55:10 +0700

    fix: android printer, support multiple instance

---
+++ android/src/main/java/me/virtualspirit/networkprinter/NPrinter.kt
@@ -0,0 +1,251 @@
+package me.virtualspirit.networkprinter
+
+
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.util.Log
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableMap
+import net.posprinter.IConnectListener
+import net.posprinter.IDeviceConnection
+import net.posprinter.POSConnect
+import net.posprinter.POSConst
+import net.posprinter.POSPrinter
+import net.posprinter.model.PTable
+import kotlinx.coroutines.*
+
+public class PTableStyle(val isBold: Int, val width: Int, val height: Int) {}
+
+class NPrinter(val host: String) {
+  private var printData = mutableListOf<Any>()
+  lateinit var  printerConnecter: IDeviceConnection
+  var promise: Promise? = null
+  private var printDensity = POSConst.SINGLE_DENSITY_8
+  private var isPrintSuccess: Boolean = false
+
+  fun appendData(data: ReadableMap) {
+    printData.add(data)
+  }
+
+  fun appendData(data: String) {
+    printData.add(data)
+  }
+
+  fun appendData(data: PTable) {
+    printData.add(data)
+  }
+
+  fun appendData(data: PTableStyle) {
+    printData.add(data)
+  }
+
+  private fun sendSuccessPromise() {
+    val params = Arguments.createMap().apply {
+      putString("status", "success")
+      putString("message", "print success")
+    }
+
+    if (promise != null) {
+      promise?.resolve(params)
+      promise = null
+    }
+  }
+
+  private fun sendErrorPromise(type: String, message: String) {
+    printData.clear()
+
+    if (promise != null) {
+      promise?.reject(type, message)
+      promise = null
+    }
+  }
+
+
+  private fun connect(callback: (() -> Unit)? = null) {
+    isPrintSuccess = false
+    printerConnecter = POSConnect.createDevice(POSConnect.DEVICE_TYPE_ETHERNET)
+    printerConnecter.connect(host, IConnectListener { code, connInfo, msg ->
+      when (code) {
+        POSConnect.CONNECT_SUCCESS -> {
+          printerConnecter.setSendCallback {
+            GlobalScope.launch {
+              isPrintSuccess = true;
+              printData.clear()
+              delay(100)
+              printerConnecter.closeSync()
+              delay(200)
+              sendSuccessPromise()
+            }
+          }
+
+          callback?.invoke()
+        }
+        POSConnect.CONNECT_FAIL -> {
+          if (!isPrintSuccess) {
+            sendErrorPromise("timeout", msg)
+          }
+        }
+        POSConnect.CONNECT_INTERRUPT -> {
+          if (!isPrintSuccess) {
+            sendErrorPromise("connection-refused", msg)
+          }
+        }
+        POSConnect.SEND_FAIL -> {
+          isPrintSuccess = false
+          sendErrorPromise("print-failure", msg)
+        }
+      }
+    })
+  }
+
+  fun disconnect() {
+    if (printerConnecter.isConnect) {
+      printerConnecter.closeSync()
+    }
+  }
+
+
+  fun print(promise: Promise) {
+    this.promise = promise
+    this.connect {
+      var printer = POSPrinter(printerConnecter).initializePrinter()
+      for (data in printData) {
+        if (data is PTableStyle) {
+          val width = getWidth(data.width)
+          val height = getHeight(data.height)
+          val bold = getBold(data.isBold)
+          printer = printer.setTextStyle(bold, width or height)
+        }
+
+        if (data is PTable) {
+          printer = printer.setAlignment(POSConst.ALIGNMENT_CENTER)
+          printer = printer.printTable(data)
+        }
+
+        if (data is String) {
+          if (data.startsWith("line-")) {
+            val regex = Regex("line-(\\d+)")
+            val match = regex.find(data)
+            val number = match?.groups?.get(1)?.value?.toInt()
+            printer = number?.let { printer.feedLine(it) }
+          }
+        }
+
+        if (data is ReadableMap) {
+          if (data.hasKey("text")) {
+            var width = 0
+            var height = 0
+            var align = POSConst.ALIGNMENT_LEFT
+            var bold = POSConst.FNT_DEFAULT
+            val text = data.getString("text")
+
+            if (data.hasKey("width")) {
+              width = getWidth(data.getInt("width"))
+            }
+
+            if (data.hasKey("height")) {
+              height = getHeight(data.getInt("height"))
+            }
+
+            if (data.hasKey("align")) {
+              align = getAlign(data.getInt("align"))
+            }
+
+            if (data.hasKey("bold")) {
+              bold = getBold(data.getInt("bold"))
+            }
+
+            printer = printer.printText("${text}\n", align, bold or printDensity, width or height )
+          }
+
+          if (data.hasKey("image")) {
+            val base64 = data.getString("image")
+            val decodedString: ByteArray = Base64.decode(base64, Base64.DEFAULT)
+            val bmp = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+
+            var width = 300
+            var align = POSConst.ALIGNMENT_LEFT
+
+            if (data.hasKey("align")) {
+              align = getAlign(data.getInt("align"))
+            }
+
+            if (data.hasKey("width")) {
+              width = data.getInt("width")
+            }
+
+            printer = printer.feedLine()
+            printer = printer.printBitmap(bmp, align, width)
+            printer = printer.feedLine()
+          }
+        }
+      }
+
+      printer = printer.feedLine(6)
+      printer = printer.cutPaper()
+    }
+  }
+
+  fun openCashBox(promise: Promise) {
+    this.promise = promise
+    this.connect {
+      POSPrinter(printerConnecter).initializePrinter().openCashBox(POSConst.PIN_TWO)
+    }
+  }
+
+  fun setDensity(density: Int) {
+    printDensity = when (density) {
+      1 -> POSConst.SINGLE_DENSITY_8
+      2 -> POSConst.SINGLE_DENSITY_24
+      3 -> POSConst.DOUBLE_DENSITY_8
+      4 -> POSConst.DOUBLE_DENSITY_24
+      else -> POSConst.SINGLE_DENSITY_8 // Default value for unknown densities
+    }
+  }
+
+  fun getWidth(width: Int): Int {
+    return when (width) {
+      1 -> 16
+      2 -> 32
+      3 -> 48
+      4 -> 64
+      5 -> 80
+      6 -> 96
+      7 -> 112
+      8 -> 128
+      else -> 0
+    }
+  }
+
+  fun getHeight(height: Int): Int {
+    return when (height) {
+      1 -> 1
+      2 -> 2
+      3 -> 3
+      4 -> 4
+      5 -> 5
+      6 -> 6
+      7 -> 7
+      8 -> 8
+      else -> 0
+    }
+  }
+
+  fun getAlign(align: Int): Int {
+    return when (NetworkPrinterCommand.fromValue(align)) {
+      NetworkPrinterCommand.ALIGN_LEFT -> POSConst.ALIGNMENT_LEFT
+      NetworkPrinterCommand.ALIGN_CENTER -> POSConst.ALIGNMENT_CENTER
+      NetworkPrinterCommand.ALIGN_RIGHT -> POSConst.ALIGNMENT_RIGHT
+      else -> POSConst.ALIGNMENT_LEFT
+    }
+  }
+
+  fun getBold(align: Int): Int {
+    return when (NetworkPrinterCommand.fromValue(align)) {
+      NetworkPrinterCommand.BOLD -> POSConst.FNT_BOLD
+      NetworkPrinterCommand.UNBOLD -> POSConst.FNT_DEFAULT
+      else -> POSConst.ALIGNMENT_LEFT
+    }
+  }
+}
--- android/src/main/java/me/virtualspirit/networkprinter/NetworkPrinterCommand.kt
+++ android/src/main/java/me/virtualspirit/networkprinter/NetworkPrinterCommand.kt
@@ -5,7 +5,10 @@ enum class NetworkPrinterCommand(val value: Int) {
     UNBOLD(2),
     ALIGN_LEFT(3),
     ALIGN_CENTER(4),
-    ALIGN_RIGHT(5);
+    ALIGN_RIGHT(5),
+    TABLE_ALIGN_ALL_LEFT(6),
+    TABLE_ALIGN_ALL_RIGHT(7),
+    TABLE_ALIGN_FIRST_LEFT(8);
 
     companion object {
         fun fromValue(value: Int): NetworkPrinterCommand? {
